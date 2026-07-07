dc2d48400cfdd823db7039f0149884d933dcff1c
Olivier Brun <contactpro@olivierbrun.eu>
2026-07-07 14:32:49 +0200

    add registration, login and email verification

--- src/Enum/DriverLevel.php
+++ src/Enum/DriverLevel.php
@@ -4,9 +4,20 @@ namespace App\Enum;
 
 enum DriverLevel: string
 {
-    case Beginner     = 'Débutant';
-    case Intermediate = 'Intermédiaire';
-    case Confirmed    = 'confirmé';
+    case Beginner     = 'beginner';
+    case Intermediate = 'intermediate';
+    case Confirmed    = 'confirmed';
     case Expert       = 'expert';
     case JoeBarTeam   = 'JoeBarTeam';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Beginner     => 'Débutant',
+            self::Intermediate => 'Intermédiaire',
+            self::Confirmed    => 'Confirmé',
+            self::Expert       => 'Expert',
+            self::JoeBarTeam   => 'Joe Bar Team',
+        };
+    }
 }
