af2cea5b605f6bbc7f7408b52b285e8f0438a8d2
Olivier Brun <contactpro@olivierbrun.eu>
2026-07-10 10:54:22 +0200

    enums : MotorcycleType, RideRhythm RideStatus

---
+++ src/Enum/MotorcycleType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Enum;
+
+enum MotorcycleType: string
+{
+    case Roadster    = 'roadster';
+    case Trail       = 'trail';
+    case Custom      = 'custom';
+    case Sport       = 'sport';
+    case SportGT     = 'sport-gt';
+    case Supermotard = 'supermotard';
+    case Sidecar     = 'sidecar';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Roadster    => 'Roadster',
+            self::Trail       => 'Trail',
+            self::Custom      => 'Custom',
+            self::Sport       => 'Sport',
+            self::SportGT     => 'Sport-GT',
+            self::Supermotard => 'Supermotard',
+            self::Sidecar     => 'Sidecar',
+        };
+    }
+}
